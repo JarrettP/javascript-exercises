f7d9ef0ff5ede90782947cab7ca58996a7daee83
Jarrett <contact@jarrettperkins.me>
2022-11-10 20:30:43 -0600

    Completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    if (num < 0) {
+        return 'ERROR';
+    }
 
+    let strOut = '';
+    let i = 0;
+
+    while (i < num) {
+        strOut = strOut + string;
+        i++;
+    }
+
+    return strOut;
 };
 
 // Do not edit below this line
