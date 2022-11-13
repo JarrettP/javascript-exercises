d5ef12e2b06ed398b564be0310adc8f404e16020
Jarrett <contact@jarrettperkins.me>
2022-11-12 19:28:27 -0600

    completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(stringIn) {
+    let stringOut = '';
+    
+    for (let i = 0; i <= stringIn.length - 1; i++){
+        stringOut = stringOut + stringIn.charAt(stringIn.length - 1 - i);
+    };
 
+    return stringOut;
 };
 
 // Do not edit below this line
