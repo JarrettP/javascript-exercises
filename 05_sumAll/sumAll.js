40aa764497a826e6141abc74982ec29e6943aa2e
Jarrett <contact@jarrettperkins.me>
2022-11-13 19:42:05 -0600

    completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let output = 0;
 
+    switch (true) {
+        case typeof startNum != 'number':
+        case typeof endNum != 'number':
+        case startNum < 0:
+        case endNum < 0:
+            return 'ERROR';
+        case startNum > endNum:
+            [startNum, endNum] = [endNum, startNum];
+            break;
+    };
+    
+    for (i = startNum; i <= endNum; i++) {
+        output = output + i
+    };
+
+    return output;
 };
 
 // Do not edit below this line
