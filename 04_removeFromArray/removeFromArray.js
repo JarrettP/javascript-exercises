7959bf4e45bb8ca37692cb1b0b070aaaa09b25e8
Jarrett <contact@jarrettperkins.me>
2022-11-13 19:07:58 -0600

    Completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
 
+    for (j = 1;j <= args.length - 1; j++) {
+        for (i = 0; i <= args[0].length - 1; i++) {
+            if (args[0][i] === args[j]) {
+                args[0].splice(i,1);
+            };
+        };
+    };
+    
+    return args[0];
 };
 
 // Do not edit below this line
