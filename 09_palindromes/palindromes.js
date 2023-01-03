4a2c3c88999b0af0d5978ed0eede612c1e9d6750
Jarrett <contact@jarrettperkins.me>
2023-01-03 11:50:11 -0600

    added reverseString function for basic palindrome check

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    //Add in a loop with a regex to remove spaces and punctuation,
+    //and also to drop all letters lower case.
+    let reverseStr = reverseString(string);
+    return reverseStr === string
+};
 
+const reverseString = function(stringIn) {
+    let stringOut = '';
+    
+    for (let i = 0; i <= stringIn.length - 1; i++){
+        stringOut = stringOut + stringIn.charAt(stringIn.length - 1 - i);
+    };
+
+    return stringOut;
 };
 
 // Do not edit below this line
