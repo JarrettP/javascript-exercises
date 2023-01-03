9b9a9cf75820408a3a00aa058cbbf5c5738cfa80
Jarrett <contact@jarrettperkins.me>
2023-01-03 11:42:18 -0600

    completed calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(...inputs) {
+	let sum = 0;
+  for (let input of inputs) sum += input;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(...inputs) {
+  return inputs[0] - inputs[1];
 };
 
-const sum = function() {
-	
+const sum = function(input) {
+	let sum = 0;
+  input.forEach(element => {
+    sum += element;    
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(input) {
+	let sum = 1;
+  input.forEach(element => {
+    sum *= element;    
+  });
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(...input) {
+  return input[0] ** input[1];
 };
 
-const factorial = function() {
-	
+const factorial = function(input) {
+	let sum = 1;
+  for (let i = input; i > 0; i--) {
+    sum *= i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
