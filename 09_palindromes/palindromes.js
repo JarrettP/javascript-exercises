2419a121915418583843be0c7d261802d7f5bf20
Jarrett <contact@jarrettperkins.me>
2023-01-03 15:29:55 -0800

    completed palindrome check function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,18 +1,13 @@
-const palindromes = function (string) {
-    //Add in a loop with a regex to remove spaces and punctuation,
-    //and also to drop all letters lower case.
-    let reverseStr = reverseString(string);
-    return reverseStr === string
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = str.replace(/\s+/g, '');
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+    let reverseStr = reverseString(str);
+    return reverseStr === str;
 };
 
-const reverseString = function(stringIn) {
-    let stringOut = '';
-    
-    for (let i = 0; i <= stringIn.length - 1; i++){
-        stringOut = stringOut + stringIn.charAt(stringIn.length - 1 - i);
-    };
-
-    return stringOut;
+const reverseString = function(str) {
+    return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
