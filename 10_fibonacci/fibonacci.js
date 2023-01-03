d988f635e6231a4809c5ce9b66b0f772504b0180
Jarrett <contact@jarrettperkins.me>
2023-01-03 12:45:55 -0600

    completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    if (input < 0) {
+        return "OOPS";
+    };
+    
+    const position = input - 1;
+    let fibonacci = [1,1];
 
+    for (let i = 0; i < position; i++) {
+        fibonacci[i + 2] = fibonacci[i] + fibonacci[i + 1];
+    };
+    return fibonacci[position];
 };
 
 // Do not edit below this line
